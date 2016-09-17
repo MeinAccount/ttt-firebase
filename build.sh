9b36b9df4074944ae041d27411beab17bc79d73c
Magnus Kühn <magnus.khn@gmail.com>
2016-09-17 10:31:44 +0200

    Fix html minification

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+elm make src/Main.elm --output src/frontend/Main.js
+uglifyjs src/frontend/Main.js src/frontend/init.js src/frontend/native.js --compress warnings=false --no-mangle > src/public/bundle.js
+uglifycss src/frontend/main.css > src/public/bundle.css
+minimize src/frontend/index.html > src/public/index.html
