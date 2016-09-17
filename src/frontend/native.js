87e2975c8e31e13c725d68498519c651c387d159
Magnus Kühn <magnus.khn@gmail.com>
2016-09-17 10:52:00 +0200

    Include firebase sdk

--- src/frontend/native.js
+++ src/frontend/native.js
@@ -1,3 +1,14 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDzx9SCOhM-nf6_0qUl5Qyn_68h9Sf-l_o",
+  authDomain: "ttt-firebase.firebaseapp.com",
+  databaseURL: "https://ttt-firebase.firebaseio.com",
+  storageBucket: "ttt-firebase.appspot.com",
+  messagingSenderId: "980310354616"
+};
+firebase.initializeApp(config);
+
+// generated content events
 app.ports.bindClick.subscribe(function() {
   var listener = function(event) {
     if (event.target.nodeName === 'TABLE') {
