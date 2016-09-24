de27084da7adf6d5ee9533b575191762ba257eaa
Magnus Kühn <magnus.khn@gmail.com>
2016-09-24 18:42:44 +0200

    Preload assets

--- src/public/init.js
+++ src/public/init.js
@@ -1,2 +1,10 @@
 var node = document.getElementById('my-app');
 var app = Elm.Main.embed(node);
+
+if ('requestIdleCallback' in window && document.images) {
+  var cross = new Image();
+	cross.src = "/assets/cross.svg";
+
+  var circle = new Image();
+	circle.src = "/assets/circle.svg";
+}
